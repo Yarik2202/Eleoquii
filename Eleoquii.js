d2aa2f33eae6ce3f524db84d093a0dfae5972450
Yaroslav <jrosuk@gmail.com>
2023-06-01 09:20:57 +0300

    toolsBar

--- Eleoquii.js
+++ Eleoquii.js
@@ -1,4 +1,19 @@
+const navButton = document.getElementById("button_nav");
+const toolsBarColumn = document.getElementById('toolsbar_link_nav_column');
+
+navButton.addEventListener("click", changeToolsbar);
 
 
 
-
+function changeToolsbar(event) {
+   event.preventDefault();
+  
+   if(navButton.value === "0") {
+      navButton.value = "1";
+      toolsBarColumn.className = "toolsbar_link_nav_column";
+   } else if (navButton.value === "1") {
+      navButton.value = "0";
+      toolsBarColumn.className = "elements_hidden";
+   }
+   
+}
